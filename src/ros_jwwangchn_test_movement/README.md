fbec7b62abd4220de65ee7a3f8a2e4cbef4426e6
jwwangchn <jwwangchn@outlook.com>
2017-09-04 22:52:00 +0800

    add ros_jwwangchn_test_movement package

---
+++ src/ros_jwwangchn_test_movement/README.md
@@ -0,0 +1,22 @@
+# ros_jwwangchn_test_movement
+
+if you are using mavros 0.12 use the branch `mavros0.12`
+
+```
+git checkout mavros0.12
+```
+
+## Compile
+
+```
+git clone https://github.com/jwwangchn/IMAV_MAVROS_F450
+cd IMAV_MAVROS_F450
+catkin_make_isolated --pkg ros_jwwangchn_test_movement
+```
+
+## How to use it
+
+```
+source devel/setup.bash
+rosrun ros_jwwangchn_test_movement ros_jwwangchn_test_movement
+```
---
+++ src/ros_jwwangchn_test_movement/src/main.cpp
@@ -0,0 +1,100 @@
+#include <cstdlib>
+
+#include <ros/ros.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/CommandTOL.h>
+#include <mavros_msgs/SetMode.h>
+
+int main(int argc, char **argv)
+{
+
+    int rate = 10;
+
+    ros::init(argc, argv, "mavros_test_movement");
+    ros::NodeHandle n;
+
+    ros::Rate r(rate);
+
+    ////////////////////////////////////////////
+    /////////////////GUIDED MODE/////////////////////
+    ////////////////////////////////////////////
+    ros::ServiceClient cl = n.serviceClient<mavros_msgs::SetMode>("/mavros/set_mode");
+    mavros_msgs::SetMode srv_setMode;
+    srv_setMode.request.base_mode = 0;
+    srv_setMode.request.custom_mode = "GUIDED";
+    if (cl.call(srv_setMode))
+    {
+        ROS_ERROR("setmode send ok %d value:", srv_setMode.response.success);
+    }
+    else
+    {
+        ROS_ERROR("Failed SetMode");
+        return -1;
+    }
+    sleep(10);
+    ////////////////////////////////////////////
+    ///////////////////ARM//////////////////////
+    ////////////////////////////////////////////
+    ros::ServiceClient arming_cl = n.serviceClient<mavros_msgs::CommandBool>("/mavros/cmd/arming");
+    mavros_msgs::CommandBool srv;
+    srv.request.value = true;
+    if (arming_cl.call(srv))
+    {
+        ROS_ERROR("ARM send ok %d", srv.response.success);
+    }
+    else
+    {
+        ROS_ERROR("Failed arming or disarming");
+    }
+
+    ////////////////////////////////////////////
+    /////////////////TAKEOFF////////////////////
+    ////////////////////////////////////////////
+    ros::ServiceClient takeoff_cl = n.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/takeoff");
+    mavros_msgs::CommandTOL srv_takeoff;
+    srv_takeoff.request.altitude = 2;
+    srv_takeoff.request.latitude = 0;
+    srv_takeoff.request.longitude = 0;
+    srv_takeoff.request.min_pitch = 0;
+    srv_takeoff.request.yaw = 0;
+    if (takeoff_cl.call(srv_takeoff))
+    {
+        ROS_ERROR("srv_takeoff send ok %d", srv_takeoff.response.success);
+    }
+    else
+    {
+        ROS_ERROR("Failed Takeoff");
+    }
+
+    ////////////////////////////////////////////
+    /////////////////DO STUFF///////////////////
+    ////////////////////////////////////////////
+    sleep(10);
+
+    ////////////////////////////////////////////
+    ///////////////////LAND/////////////////////
+    ////////////////////////////////////////////
+    ros::ServiceClient land_cl = n.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/land");
+    mavros_msgs::CommandTOL srv_land;
+    srv_land.request.altitude = 2;
+    srv_land.request.latitude = 0;
+    srv_land.request.longitude = 0;
+    srv_land.request.min_pitch = 0;
+    srv_land.request.yaw = 0;
+    if (land_cl.call(srv_land))
+    {
+        ROS_INFO("srv_land send ok %d", srv_land.response.success);
+    }
+    else
+    {
+        ROS_ERROR("Failed Land");
+    }
+
+    while (n.ok())
+    {
+        ros::spinOnce();
+        r.sleep();
+    }
+
+    return 0;
+}
